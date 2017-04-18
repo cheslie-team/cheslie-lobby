e32cb27965856a5759098e0069bd8d9309e3df7d
teodoran <teodor@andeelstad.com>
2017-04-18 03:10:49 +0200

    Joining game in wrong order

--- lobby.js
+++ lobby.js
@@ -72,10 +72,10 @@ io.on('connect', function (socket) {
 			state: 'starting'
 		});
 
-		socket.broadcast.to(white.id).emit('join', gameId);
+		socket.broadcast.to(black.id).emit('join', gameId);
 		// Temporary hack to control who gets to be white and black
 		setTimeout(function () {
-			socket.broadcast.to(black.id).emit('join', gameId);
+			socket.broadcast.to(white.id).emit('join', gameId);
 		}, 1000);
 	});
 
